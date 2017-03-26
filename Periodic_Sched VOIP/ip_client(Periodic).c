77c40a17a4ffafb1668208d21d0f9a56927ec181
nmdesai92 <nihar.desai1@gmail.com>
2017-03-26 08:41:47 +0530

    Periodic Scheduler
    client  :This uses create_timer() API to schedule a task of record and send data at every 20ms period.
    Server :This uses create_timer() API to schedule a task of receive and play data at every 20ms period.
    
    Bandwidth measured is ~1 KB/s.

---
+++ Periodic_Sched VOIP/ip_client(Periodic).c
@@ -0,0 +1,159 @@
+/*Client for VOIP with periodic scheduler*/
+/*Command line arguments: [IP of Server] [Port Address]
+/*Press CTRL+C to disconnect the call*/
+
+/*Github Link: https://github.com/nmdesai92/ip-phone*/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <time.h>
+#include <sys/time.h>
+
+#include <pulse/simple.h>
+#include <pulse/error.h>
+
+#define BUFSIZE 4096
+
+#define CLOCKID CLOCK_REALTIME
+#define SIG SIGRTMIN
+
+/*Global variables*/
+pa_simple *s = NULL;
+int error;
+
+int socket_desc;
+
+/*Continuously send data over Socket*/
+static ssize_t loop_send(int fd, const void *data, size_t size)
+{
+	ssize_t ret  = 0;
+
+	while(size > 0)
+	{
+		ssize_t r;
+
+		if((r = send(fd, data, size,0)) < 0)
+		{
+			return r;
+		}
+		if(r == 0)
+		{
+			break;
+		}
+
+		ret += r;
+		data = (const uint8_t *)data + r;
+		size -= (size_t) r;
+	}
+	return ret;
+}
+
+/*Timer handler to record and send data*/
+void timer_handler(int signum)
+{
+		uint8_t buf[BUFSIZE];
+
+		if(pa_simple_read(s, buf, sizeof (buf), &error) < 0)
+		{
+			fprintf(stderr,__FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+			if(s) pa_simple_free(s);
+			exit(1);
+		}
+		/*send data...*/
+		if(loop_send(socket_desc, buf, sizeof(buf)) != sizeof(buf))
+		{
+			perror("send failed:");
+		}
+}
+
+int main(int argc,char*argv[])
+{
+	/* The Sample type to use*/
+	static const pa_sample_spec ss = {
+		.format = PA_SAMPLE_S16LE,
+		.rate = 44100,
+		.channels = 2
+	};
+
+	
+	struct sockaddr_in server;
+
+	/*Timer parameters*/
+	timer_t timerid;
+    struct sigevent sev;
+    struct itimerspec its;
+    struct sigaction sa;
+
+    /*Timer signal setting*/
+    memset (&sa, 0, sizeof (sa));
+    sa.sa_handler = timer_handler;
+    sigaction(SIG, &sa, NULL);
+
+	
+
+	/*Create recording stream*/
+
+	s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL,NULL, &error);
+
+	if(!s)
+	{
+		fprintf(stderr, __FILE__" : pa_simple_new() failed : %s\n", pa_strerror(error));
+		exit(1);
+	}
+
+	/*create socket*/
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_desc == -1)
+	{
+		printf("Could not create Socket");
+	}
+
+	server.sin_addr.s_addr = inet_addr(argv[1]);
+	server.sin_family = AF_INET;
+	server.sin_port = htons( atoi(argv[2]));
+
+	/*connect to remote server*/
+
+	if((connect(socket_desc, (struct sockaddr *)&server, sizeof server)) < 0)
+	{
+		perror("bind faild");
+		puts("connect error");
+		exit(1);
+	}
+
+	/*record audio*/
+
+	sev.sigev_notify = SIGEV_SIGNAL;
+    sev.sigev_signo = SIG;
+    sev.sigev_value.sival_ptr = &timerid;
+
+    /*Create Timer*/
+    if (timer_create(CLOCKID, &sev, &timerid) == -1)
+         perror("timer_create");
+
+    /*Set values : Period = 20ms*/
+    its.it_value.tv_sec = 0 ;
+    its.it_value.tv_nsec =  20;
+    its.it_interval.tv_sec = 0;
+    its.it_interval.tv_nsec = 20000000;
+
+    /*Start Timer*/
+    if (timer_settime(timerid, 0, &its, NULL) == -1)
+              perror("timer_settime");
+
+    while(1);	//Start Schedulin
+
+    return 0;
+
+
+}
---
+++ Periodic_Sched VOIP/ip_server(Periodic).c
@@ -0,0 +1,180 @@
+/*Srrver for VOIP with periodic scheduler*/
+/*Port number for this particular server is 5000*/
+
+/*Github Link: https://github.com/nmdesai92/ip-phone*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#include <time.h>
+#include <sys/time.h>
+
+#include <pulse/simple.h>
+#include <pulse/error.h>
+
+#define PORT "5000"
+#define BUFSIZE 4096
+
+
+#define CLOCKID CLOCK_REALTIME
+#define SIG SIGRTMIN
+
+/*Global variables*/
+pa_simple *s = NULL;
+int error,new_socket;
+
+uint8_t buf[BUFSIZE];
+ssize_t r;
+
+/*Timer handler to receive and play*/
+static void timer_handler (int signum)
+{
+
+			/* receive audio data...*/
+		if((r = recv(new_socket, buf, sizeof(buf), 0)) <= 0)
+		{
+			if(r == 0)	/*Disconnect call if client is off*/
+			{
+					printf("call disconnected\n");
+					exit(0);
+
+			}
+			perror("read() failed :");
+			if(s)	pa_simple_free(s);
+			exit(1);
+		}
+		/*play audio...*/
+		 if (pa_simple_write(s, buf, (size_t) r, &error) < 0) {
+        	perror("pa_simple_write() failed:");
+        	if(s)	pa_simple_free(s);
+    	}
+
+    	/*Make sure that every single sample is played*/
+		if(pa_simple_drain(s, &error) < 0)
+		{
+			perror("pa_simple_drain() failed :");
+			if(s)	pa_simple_free(s);
+			exit(1);
+		}	
+	
+}
+
+int main(int argc,char **argv)
+{
+	/*The sample format to use*/
+	static const pa_sample_spec ss = {
+		.format = PA_SAMPLE_S16LE,
+		.rate = 44100,
+		.channels = 2
+	};
+
+
+	int socket_desc, c;
+	struct addrinfo hints, *server, *p;
+	struct sockaddr_in client;
+	int rv;
+
+	  timer_t timerid;
+      struct sigevent sev;
+      struct itimerspec its;
+      struct sigaction sa;
+
+      /* Timer interrupt */
+      memset (&sa, 0, sizeof (sa));
+      sa.sa_handler = timer_handler;
+      sigaction(SIG, &sa, NULL);
+
+	
+	memset(&hints, 0,sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	/*Create a new Playback stream*/
+	s = pa_simple_new(NULL, argv[0],PA_STREAM_PLAYBACK,NULL,"Playback", &ss,NULL,NULL,&error);
+	if(!s)
+	{
+		perror("pa_simple_new failed()");
+		if(s)	pa_simple_free(s);
+		exit(1);
+	}
+
+	/*get details of server*/
+	if((rv = getaddrinfo(NULL, PORT, &hints, &server)) != 0)
+	{
+		perror("getaddrinfo() :");
+		exit(1);
+	}
+
+	p = server;
+
+	/*create Socket*/
+	socket_desc = socket(p->ai_family,p->ai_socktype,p->ai_protocol);
+	if(socket_desc == -1)
+	{
+		printf("could not create socket");
+	}
+
+	/*Bind to socket*/
+	if(bind(socket_desc,p->ai_addr,p->ai_addrlen) < 0)
+	{
+		close(socket_desc);
+		perror("bind failed");
+		exit(1);
+	}
+
+	puts("bind done");
+
+	freeaddrinfo(server);
+	
+	/*Listen*/
+	listen(socket_desc, 10);
+
+	puts("Waiting for incoming calls...");
+
+	c = sizeof(struct sockaddr_in);
+
+	/*accept the connection*/
+	new_socket = accept(socket_desc,(struct sockaddr *)&client,(socklen_t *)&c);
+	if(new_socket == -1)
+	{
+		perror("accept");
+	}
+	puts("connection accepted");
+
+	  sev.sigev_notify = SIGEV_SIGNAL;
+	  sev.sigev_signo = SIG;
+	  sev.sigev_value.sival_ptr = &timerid;
+	  
+	 /*Create timer*/ 
+	if (timer_create(CLOCKID, &sev, &timerid) == -1)
+	     perror("timer_create");
+
+	 /*Set values : period = 20 ms*/
+    its.it_value.tv_sec = 0 ;
+    its.it_value.tv_nsec =  20;
+    its.it_interval.tv_sec = 0;
+    its.it_interval.tv_nsec = 20000000;		//20ms
+
+    /*Start the 20ms timer*/
+    if (timer_settime(timerid, 0, &its, NULL) == -1)
+              perror("timer_settime");  
+    
+    while(1);	//Schedule timer
+   
+   	return 0;
+}
